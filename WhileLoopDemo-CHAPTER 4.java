8c514e66b73f820f440c7abec8e136b7bbb7b7a0
roth0 <87845634+roth0@users.noreply.github.com>
2024-11-15 11:24:14 +0800

    Add files via upload

---
+++ WhileLoopDemo-CHAPTER 4.java
@@ -0,0 +1,22 @@
+package whileloopdemo;
+
+public class WhileLoopDemo {
+
+    public static void main(String[] args) {
+       
+        int count = 1;  
+        System.out.println("While Loop : ");
+        System.out.println("Printing Number from 1 to 10");
+        
+        while (count <= 10) {
+            System.out.println(count++);  
+            
+            
+             System.out.println("Do While Loop : ");
+            
+            
+            
+            
+        }
+    }
+}
