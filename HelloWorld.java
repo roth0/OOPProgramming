763a6fd376daf31c0a47db4f28cf6ae6fddd43ce
roth0 <87845634+roth0@users.noreply.github.com>
2024-11-15 11:02:30 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author User
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    //End of HelloWorld Class
+}
+    //End of main   
