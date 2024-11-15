2d77a5a36a812e0c4dc2bcd0d094a66df9f0cbfa
roth0 <87845634+roth0@users.noreply.github.com>
2024-11-15 11:26:28 +0800

    Add files via upload

---
+++ ForLoopDemo-CHAPTER 4.java
@@ -0,0 +1,12 @@
+package forloopdemo;
+
+public class ForLoopDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Printing Number from 1 to 10");
+        
+        for (int count = 1; count <= 10; count++) {
+            System.out.println(count);
+        }
+    }
+}
